aeb912ab46c739de00616d0dc858cfabd68719f0
Andres Balaguera <andres-balaguera@hotmail.com>
2023-09-04 18:52:56 -0500

    clase logica2

---
+++ conversiones.py
@@ -0,0 +1,25 @@
+#dato_ingresado = input("Digite su edad:")
+#print(dato_ingresado + 1)
+
+#dato_convertido = int(dato_ingresado)
+#print(dato_convertido + 1)
+
+## TALLER, CONVERTIR DOLARES A PESOS COLOMBIANOS
+## TRM = 4,111.52
+#Un programa que le pida al usuario que le pida el valor a dolares y el programa lo convierta a valores en pesos
+
+#dato_ingresado_dolar = input("Digite la cantidad de dolares: ")
+#dato_convertido_dolar = float(dato_ingresado_dolar)
+#print(dato_convertido_dolar * 4111.52)
+
+#consTrm = 4111.52
+#valorUSD = float(input("Digite el valor en dolar (USD): "))
+#print(valorUSD * consTrm)
+
+#str(123456)
+#int("23")
+#float("85.26")
+print(bool(0))
+print(bool(""))
+print(bool(False))
+print(bool(None))
